fa1d0fb71c2c5006e997383879e3b850df8b240e
yoshitaka motomura <stay.daybreak@gmail.com>
2020-06-30 21:26:40 +0900

    Added. react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 const App: React.FC = () => {
-  return <div className="App">It`s works</div>
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/about" component={About}></Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
